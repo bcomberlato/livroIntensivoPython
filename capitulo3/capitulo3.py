f70fd2ae96b27b727f40e36980bc7b8e0e268b8e
bruno <bcomberlato@gmail.com>
2021-10-25 18:25:19 -0300

    Initial commit

---
+++ capitulo3/capitulo3.py
@@ -0,0 +1,15 @@
+class capitulo3:
+    paises_mundo = ['canada', 'alemanha', 'suiça', 'inglaterra','eua']
+    print(paises_mundo)
+    print(sorted(paises_mundo))
+    print (sorted(paises_mundo, reverse=True))
+    paises_mundo.reverse()
+    print(paises_mundo)
+    paises_mundo.reverse()
+    print(paises_mundo)
+    paises_mundo.sort()
+    print(paises_mundo)
+    paises_mundo.sort(reverse=True)
+    print(paises_mundo)
+
+    
