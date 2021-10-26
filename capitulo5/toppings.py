189fb6ba01fcc4ae7668244be94c54b95437690b
bruno <bcomberlato@gmail.com>
2021-10-25 22:05:20 -0300

    implementando o capitulo 5

---
+++ capitulo5/toppings.py
@@ -0,0 +1,7 @@
+requested_topping = 'mushrooms'
+if requested_topping != 'achovies':
+    print('Hold the anchovies')
+
+requested_topping = ['mushrooms', 'onions', 'pineapple']
+if 'mushrooms' in requested_topping:
+    print('tem cogumelo')
---
+++ capitulo5/voting.py
@@ -0,0 +1,7 @@
+age = 19
+if age >= 18:
+    print("You are old enough to vote")
+    print("Have you registred to vote yet?")
+else:
+    print("Sorry, you are to young to vote")
+    print("Please register to vote as soon you turn 18!")
